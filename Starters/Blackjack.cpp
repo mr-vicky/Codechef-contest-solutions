ae27a1e0e459ed52ca21759419025a2faf74c167
Vicky <viveksarade4949@gmail.com>
2022-03-15 19:39:50 +0530

    modified Blackjack Program

--- Starters/Blackjack.cpp
+++ Starters/Blackjack.cpp
@@ -10,18 +10,7 @@ int main()
         int x, y, z;
         cin>>x>>y;
         z = 21-(x+y);
-<<<<<<< HEAD
         (z >= 1 && z <= 10)?cout<<"z\n":cout<<"-1\n";
-=======
-        if(z >= 1 && z <= 10)
-        {
-            cout<<z<<endl;
-        }
-        else
-        {
-            cout<<-1<<endl;
-        }
->>>>>>> f6fe0db23363ac8977af5c2d62620290d5c0a704
     }
     return 0;
 }
